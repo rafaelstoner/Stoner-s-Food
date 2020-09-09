d401d117338d09c3589d62b7df2cc746b6b2eb8a
Rafael Stoner <rafaelstoner7@gmail.com>
2020-09-09 16:27:15 +0200

    just maid the buttons and navigation work

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     
+    /*Sticky Navigation */
      $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -8,6 +9,36 @@ $(document).ready(function() {
             $('nav').removeClass('sticky');
         }
          
+    });
+    
+    
+    /* Scroll on Buttons*/
+    
+    $('.js--scrool-to-plan').click(function(){
+        
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000) ;
+    }); 
+    
+    $('.js--scrool-to-start').click(function(){
+        
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000) ;
+    });
+    
+    /*Navigation Scrool*/
+     $(function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                 
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
     });
   
 });
