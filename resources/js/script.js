f588b7bf56c62357e472fa1ad388d92015bace30
Rafael Stoner <rafaelstoner7@gmail.com>
2020-09-10 00:37:41 +0200

    Build a responsive navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -94,5 +94,20 @@ $('a[href*="#"]')
        offset: '50%'
    });
     
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i ')
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+        ion-close-round
+    })
   
 });
